e657404b9b3e4538ffa4ab16ae8bb33236b2ccd8
Tallak Tveide <tt@kitemill.no>
2022-02-27 16:31:08 +0100

    More sane parameters for October kite geometry

--- src/kites/october.jl
+++ src/kites/october.jl
@@ -3,11 +3,11 @@ function october_kite()
          , m = 1852.0
          , s = 54.0
          , d = 0.04
-         , l = 160.0
+         , l = 250.0
          , design_c_l = 2.0
          # the glide number seems very high
          , c_d_fun_coeffs = [0.00436647190519774, 0.016732036920022945, -0.012590655090473097, 0.004275035381646313, -0.0002828891163939005]
          , elev = deg2rad(30.0)
-         , radius = 80.0 # wingspan 26 m
+         , radius = 100.0 # wingspan 26 m
         )
 end
